b9f736a8950dd712d0b658c594721d9ba4048a0b
pushpak1497 <150467635+pushpak1497@users.noreply.github.com>
2024-10-29 15:31:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      "/api":"https://book-app-backend-kjpm.onrender.com"
+    }
+  }
   plugins: [react()],
+  
 })
